80e8e7503fe32ae599cf4d24fbaaefca200cbaec
kennedylsodre <klacerda88@gmail.com>
2026-03-17 20:49:06 -0300

    Primeiro upload de teste realizado com sucesso

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv 
 import os
+import boto3
 
 load_dotenv() 
 
@@ -7,3 +8,13 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_REGION = os.getenv('AWS_REGION')
 BUCKET_NAME = os.getenv('BUCKET_NAME')
+
+
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_REGION
+)
+
+s3_client.upload_file('requirements.txt', BUCKET_NAME, 'requirements.txt')
